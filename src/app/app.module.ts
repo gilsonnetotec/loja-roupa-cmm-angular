55f0a5ab2848d05f0481b56705717dd272f053e2
Gilson José Correia da Silva Neto <126289377+gilsonnetotec@users.noreply.github.com>
2023-08-20 23:24:35 +0000

    Sistema de search começando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './modules/home/home.module';
 import { CustomerModule } from './modules/customer/customer.module';
+import { PainelHeaderComponent } from './core/components/painel-header/painel-header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent, 
+    PainelHeaderComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, HomeModule, CustomerModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/modules/customer/page/customer/customer.component.ts
+++ src/app/modules/customer/page/customer/customer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-customer',
   templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.css']
+  styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent {
 
